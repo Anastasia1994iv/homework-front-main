504c78887fc3a9503a9fb6f9f595be4c5cd0d52a
Anastasia <lovly7@yandex.ru>
2024-08-03 20:03:07 +0300

    hw-01-03

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -12,7 +12,6 @@ export const HW3 = () => {
 
   // ❗ Массив с данными не трогаем! Текст не меняем !
 
-
   const [currentText, setCurrentText] = useState('');
   const [texts, setTexts] = useState<string[]>([
     'То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)',
@@ -31,28 +30,28 @@ export const HW3 = () => {
   return (
     <div id={'hw03'}>
       {currentText ? (
-       <h1 id={'hw03-text'}>{currentText}</h1>
+        <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
-       <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
+        <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
       )}
 
-      <input id={'hw03-input'} type="text" value={currentText} 
-      onChange={(e)=>handleChange(e.currentTarget.value)} />
+      <input id={'hw03-input'} type="text" value={currentText}
+        onChange={(e) => handleChange(e.currentTarget.value)} />
 
-      <button id={'hw03-button'} onClick={handleSave}> 
-       Сохранить
+      <button id={'hw03-button'} onClick={handleSave}>
+        Сохранить
       </button>
 
       <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
       <ol id={'hw03-tasks'}>
-       {texts.map((el, index) => {
-         return (
-           <li key={index} id={`hw03-task-${index}`}>
-             {el}
-           </li>
-         );
-       })}
+        {texts.map((el, index) => {
+          return (
+            <li key={index} id={`hw03-task-${index}`}>
+              {el}
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
